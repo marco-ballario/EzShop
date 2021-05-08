b5aa0a432ee9584a8b7be2aacda0be6fefc34cb5
LUCIA VENCATO <s247922@studenti.polito.it>
2021-05-08 16:46:11 +0200

    feat: implement validation algorithm

--- src/main/java/it/polito/ezshop/data/EZShop.java
+++ src/main/java/it/polito/ezshop/data/EZShop.java
@@ -143,7 +143,6 @@ public class EZShop implements EZShopInterface {
 	      }
 	}
 	
-	
 	@SuppressWarnings("unchecked")
 	private ArrayList<Long> readLoyalty() {;
 		ArrayList<Long> cardsNo = null;
@@ -174,6 +173,59 @@ public class EZShop implements EZShopInterface {
 	      } catch (IOException i) {
 	         return false;
 	      }
+	}
+	
+	private boolean checkDigit(String code) {
+		int tmp=0, tot=0;
+		int lastCodeDigit = Character.getNumericValue(code.charAt(code.length()-1));  //last code digit, used as check number
+		int len = code.length();
+		
+		//multiplication and sum of all the digit except the last one
+		if(len == 12 || len == 14) {
+			for(int i=len-2; i>=0; i--) {
+				tmp=1;
+				if(i%2 != 0) {         //if the position is odd the number must be multiplied by 3, if even by 1
+					tmp = Character.getNumericValue(code.charAt(i));
+					tot += tmp*3;
+				}
+				else {
+					tmp = Character.getNumericValue(code.charAt(i));
+					tot += tmp;
+				}	
+			}
+		}
+		
+		if(len == 13) {
+			for(int i=len-2; i>=0; i--) {
+				tmp=1;
+				if(i%2 == 0) {         //if the position is even the number must be multiplied by 3, if odd by 1
+					tmp = Character.getNumericValue(code.charAt(i));
+					tot += tmp*3;
+				}
+				else {
+					tmp = Character.getNumericValue(code.charAt(i));
+					tot += tmp;
+				}	
+			}
+		}
+		
+		//Subtract the sum from nearest equal or higher multiple of ten
+		int i=1, mul=0, flag=0;
+		while(flag==0) {
+			mul=10*i;
+			i++;
+			if(tot<=mul)
+				flag=1;
+		}
+		
+		//calculate the last digit, which is the check digit of the barCode
+		int check = mul-tot;
+		
+		//if check digit is equal to the last digit of the code, that's valid otherwise not
+		if( check == lastCodeDigit ) 
+			return true;
+		else 
+			return false;
 	}
 
 	@Override
@@ -309,7 +361,9 @@ public class EZShop implements EZShopInterface {
 		}catch(NumberFormatException e) {
 			throw new InvalidProductCodeException();
 		}
-    	// to do
+    	
+    	if(checkDigit(productCode) == false)
+    		throw new InvalidProductCodeException();
     	
     	if(pricePerUnit <= 0)
     		throw new InvalidPricePerUnitException();
@@ -325,7 +379,7 @@ public class EZShop implements EZShopInterface {
         		return -1;
         }
     	
-    	it.polito.ezshop.model.ProductType pt = new it.polito.ezshop.model.ProductType(description, productCode, pricePerUnit, note);
+    	ProductType pt = new it.polito.ezshop.model.ProductType(description, productCode, pricePerUnit, note);
     	pt.setId(this.productId);
     	this.productList.put(this.productId, pt);
     	
@@ -349,10 +403,13 @@ public class EZShop implements EZShopInterface {
     	if(newCode == null || newCode.isEmpty()) 
     		throw new InvalidProductCodeException();
     	try{
-			Integer.parseInt(newCode);
+    		Long.parseLong(newCode);
 		}catch(NumberFormatException e) {
-			throw new InvalidProductCodeException();   //TODO barcode valido --> algoritmo
+			throw new InvalidProductCodeException();   
 		}
+    	
+    	if(checkDigit(newCode) == false)
+    		throw new InvalidProductCodeException();
     	
     	if(newPrice <= 0)
     		throw new InvalidPricePerUnitException();
@@ -404,7 +461,7 @@ public class EZShop implements EZShopInterface {
     	if(this.loggedUser == null || (!this.loggedUser.getRole().equals("Administrator") && !this.loggedUser.getRole().equals("ShopManager") && !this.loggedUser.getRole().equals("Cashier")))
     		throw new UnauthorizedException();
     	
-    	return new LinkedList<it.polito.ezshop.data.ProductType>();
+    	return new LinkedList<ProductType>(productList.values());
     }
 
     @Override
@@ -415,10 +472,13 @@ public class EZShop implements EZShopInterface {
     	if(barCode == null || barCode.isEmpty()) 
     		throw new InvalidProductCodeException();
     	try{
-			Integer.parseInt(barCode);
+    		Long.parseLong(barCode);
 		}catch(NumberFormatException e) {
-			throw new InvalidProductCodeException();   //TODO barcode valido
+			throw new InvalidProductCodeException(); 
 		}
+    	
+    	if(checkDigit(barCode) == false)
+    		throw new InvalidProductCodeException();
     	
     	for(ProductType p : productList.values()) {
     		if(p.getBarCode().equals(barCode))
